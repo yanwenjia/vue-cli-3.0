f02560e344b2e0128b4621caa44ea6c6b7d04a50
jiayanwen <714348906@qq.com>
2019-10-24 17:50:21 +0800

    save

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link :to="route.path" v-for="route in routes" v-bind:key="route">{{route.name}}</router-link>
+    </div>
+    <div class="router-view">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+<script>
+import { routes } from "./router/index";
+export default {
+  data() {
+    return {
+      routes
+    };
+  }
+};
+</script>
+
+<style lang="less">
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  min-height: 100%;
+}
+
+#nav {
+  padding: 30px;
+  flex-basis: 20%;
+  min-height: 100%;
+  border-right: 1px solid #ddd;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    display: block;
+    line-height: 50px;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+.router-view {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+}
+</style>
---
+++ src/views/v-if.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="show">点击我可以切换显示隐藏</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: true
+    };
+  },
+  methods: {
+    change: function() {
+      this.show = !this.show;
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
