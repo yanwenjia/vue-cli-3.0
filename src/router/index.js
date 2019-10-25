69b6434a5fd5daa59939a2e53eef59fbad7dc5cc
jiayanwen <714348906@qq.com>
2019-10-25 11:24:14 +0800

    v-for

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,22 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // import Home from '../views/Home.vue'
 import Vif from '../views/v-if.vue'
+import Vfor from '../views/v-for.vue'
 
 Vue.use(VueRouter)
 
-export const routes = [{
-  path: '/v-if',
-  name: 'v-if',
-  component: Vif
-}, ]
+export const routes = [
+  {
+    path: '/v-if',
+    name: 'v-if',
+    component: Vif
+  },
+  {
+    path: '/v-for',
+    name: 'v-for',
+    component: Vfor
+  },
+]
 
 const router = new VueRouter({
   mode: 'history',
---
+++ src/views/v-for.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="out">
+    <div class="inner" v-for="person in list" v-bind:key="person.id">
+        这是{{person.name}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            list:[
+                {
+                    id:1,
+                    name:"路人甲"
+                },
+                {
+                    id:2,
+                    name:"路人乙"
+                }
+            ]
+        }
+    }
+};
+</script>
+
+<style>
+</style>
